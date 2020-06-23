b94e43895fca8f0b24ab975e4a1553dbafaf528c
p_rqpeng <p_rqpeng>
2020-06-23 16:03:14 +0800

    feat(dialog): update dialog api
    add a props: close

--- examples/dialog/demos/others.vue
+++ examples/dialog/demos/others.vue
@@ -4,8 +4,18 @@
     <t-button theme="primary" @click="()=>this.visibleBody = true">挂载在body</t-button>
     <t-button theme="primary" @click="()=>this.visibleIdAttach = true">挂载特定元素</t-button>
     <t-button theme="primary" @click="()=>this.visibleFunctionAttach = true">挂载函数返回节点</t-button>
-    <t-dialog v-model="visibleNotModal" mode="not-modal" draggable></t-dialog>
-    <t-dialog v-model="visibleBody" attach="body" header="全局对话框">
+    <t-dialog
+      v-model="visibleNotModal"
+      :close="()=>this.visibleNotModal = false"
+      mode="not-modal"
+      draggable
+    ></t-dialog>
+    <t-dialog
+      v-model="visibleBody"
+      :close="()=>this.visibleBody = false"
+      attach="body"
+      header="全局对话框"
+    >
       <div slot="body">
         <div>我是被挂载到body元素的对话框</div>
         <div>我是内容</div>
@@ -14,7 +24,12 @@
         <div>我是内容</div>
       </div>
     </t-dialog>
-    <t-dialog v-model="visibleIdAttach" attach="#app" header="挂载到id为app的元素">
+    <t-dialog
+      v-model="visibleIdAttach"
+      :close="()=>this.visibleIdAttach = false"
+      attach="#app"
+      header="挂载到id为app的元素"
+    >
       <div slot="body">
         <div>通过querySelect指定元素挂载</div>
         <div>支持原生document.uerySelect选择元素</div>
@@ -23,7 +38,12 @@
         <div>我是内容</div>
       </div>
     </t-dialog>
-    <t-dialog v-model="visibleFunctionAttach" :attach="getAttach" header="函数返回挂载节点">
+    <t-dialog
+      v-model="visibleFunctionAttach"
+      :close="()=>this.visibleFunctionAttach = false"
+      :attach="getAttach"
+      header="函数返回挂载节点"
+    >
       <div slot="body">
         <div>指定函数返回的节点为挂载点</div>
         <div>函数返回为DOM节点对象</div>
