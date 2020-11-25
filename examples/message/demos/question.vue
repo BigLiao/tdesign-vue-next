f6ba989eca946c46479df5cc1b72c4b934679108
v_vmlgong(龚模良) <v_vmlgong()>
2020-11-25 10:34:18 +0800

    message demo 优化

--- examples/message/demos/question.vue
+++ examples/message/demos/question.vue
@@ -4,6 +4,7 @@
     <t-message theme="question" v-if="closableMsg" closeBtn @click-close-btn="closableMsg = false"
       >用于帮助用户操作的信息提示, 可关闭, 通常信息较长</t-message
     >
+    <t-button :disabled="closableMsg" @click="closableMsg = !closableMsg">显示关闭信息</t-button>
   </div>
 </template>
 
--- examples/message/demos/success.vue
+++ examples/message/demos/success.vue
@@ -4,6 +4,7 @@
     <t-message theme="success" v-if="closableMsg" closeBtn @click-close-btn="closableMsg = false"
       >用于表示操作顺利达成, 可关闭, 通常信息较长</t-message
     >
+     <t-button :disabled="closableMsg" @click="closableMsg = !closableMsg">显示关闭信息</t-button>
   </div>
 </template>
 
