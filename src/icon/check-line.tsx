a263fa378fb6cb40476642b048445023f47f3a19
xiaosansiji <799552341@qq.com>
2020-12-18 20:22:29 +0800

    feat: size default: middle -> medium

--- src/icon/check-line.tsx
+++ src/icon/check-line.tsx
@@ -8,14 +8,14 @@ export default Vue.extend({
   name,
 
   props: {
-    // xs/small/middle/large/xl/18px/2em
+    // xs/small/medium/large/xl/18px/2em
     size: {
       type: String,
     },
   },
 
   computed: {
-    classes(): Array<string|object> {
+    classes(): Array<string | object> {
       return [
         `${prefix}-icon`,
         name,
@@ -23,7 +23,7 @@ export default Vue.extend({
       ];
     },
     iconStyle(): object {
-      if (['xs', 'small', 'middle', 'large', 'xl'].includes(this.size)) return {};
+      if (['xs', 'small', 'medium', 'large', 'xl'].includes(this.size)) return {};
       return {
         'font-size': this.size,
       };
@@ -32,10 +32,10 @@ export default Vue.extend({
 
   render() {
     return (
-            <svg class={this.classes} style={this.iconStyle}  viewBox="0 0 1024 1024" version="1.1"
-                 xmlns="http://www.w3.org/2000/svg">
-                <path d="M283.568 489.36l45.264-45.264 135.76 135.76L690.88 353.6l45.264 45.264L509.84 625.12l-45.248 45.248z" p-id="4547"></path>
-            </svg>
+      <svg class={this.classes} style={this.iconStyle} viewBox="0 0 1024 1024" version="1.1"
+        xmlns="http://www.w3.org/2000/svg">
+        <path d="M283.568 489.36l45.264-45.264 135.76 135.76L690.88 353.6l45.264 45.264L509.84 625.12l-45.248 45.248z" p-id="4547"></path>
+      </svg>
     );
   },
 });
