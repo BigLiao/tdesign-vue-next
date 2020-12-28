d64c2b975393df1b2b00a8ccb1d1d5baebbafa6a
PsTiu <20109955@qq.com>
2020-12-28 11:46:23 +0800

    chore: 日历组件引入dayjs，使用d.ts，干掉大部分的any，简化了部分代码（后续要继续优化）

---
+++ examples/calendar/demos/test.vue
@@ -0,0 +1,18 @@
+<template>
+  <t-calendar></t-calendar>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      value: '1998/11/11',
+    };
+  },
+  created() {
+    setTimeout(() => {
+      this.value = new Date(2000, 11, 25);
+    }, 3000);
+  },
+};
+</script>
