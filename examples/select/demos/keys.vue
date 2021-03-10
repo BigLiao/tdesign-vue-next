d81626f148759071547c75d21053e02a33c62eaf
geff1991 <geff1991@yeah.net>
2021-03-10 20:17:36 +0800

    feature/select-api-review (merge request !327)
    Squash merge branch 'feature/select-api-review' into 'develop'
    
    * fix: update unit test
    
    * fix: update unit test
    
    * fix: lint error
    
    * fix: lint error
    
    * feat: update test unit
    
    * feat(select): 优化代码样式
    
    * feat: select API  issuse 144,237
    
    * docs(select): remove triggerText
    
    * docs(select): update docs
    
    * feat(select): change popup export
    
    * docs(select): update api.md
    
    * docs(select): remove suffixIcon API
    
    * docs(select): generate types/props/api.md
    
    * fix(select): options value为空时显示问题
    
    * feat: select API  issuse 144,237
    
    * docs(select): 情况 -> 清除
    
    * docs(select): remove triggerText
    
    * docs(select): update docs
    
    * docs(select): update api.md
    
    * docs(select): remove suffixIcon API
    
    * docs(select): generate types/props/api.md
    
    * feat: update test unit
    
    * feat(select): deal conflict
    
    * feat(select): 优化代码样式
    
    * fix: deal conflict
    
    * fix(select): options value为空时显示问题
    
    * feat: select API  issuse 144,237
    
    * docs(select): 情况 -> 清除
    
    * docs(select): remove triggerText
    
    * docs(select): update docs
    
    * feat(select): change popup export
    
    * docs(select): update api.md
    
    * docs(select): remove suffixIcon API
    
    * docs(select): update docs
    
    * docs(select): update docs
    
    * docs(select): generate types/props/api.md
    
    * fix: deal conflict
    
    * fix(select): options value为空时显示问题
    
    * feat(select): merge处理auto select分支，使用自动生成的api
    
    * feat: select API  issuse 144,237
    
    * docs(select): 情况 -> 清除
    
    * docs(select): remove triggerText
    
    * docs(select): update docs
    
    * feat: merge
    
    * feat(select): change popup export
    
    * docs(select): update api.md
    
    * docs(select): remove suffixIcon API
    
    * docs(select): update docs
    
    * docs(select): update docs
    
    * feat(select): merge
    
    * docs(select): generate types/props/api.md

---
+++ examples/select/demos/keys.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <t-select
+      v-model="value"
+      class="demo-select-base"
+      clearable
+      :keys="{value: 'data', label:'text'}"
+      @change="handleChange"
+      @visible-change="visibleChange"
+    >
+      <t-option
+        v-for="(item, index) in options"
+        :value="item.data"
+        :label="item.text"
+        :key="index"
+      >
+        {{ item.text }}
+      </t-option>
+    </t-select>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      options: [
+        {
+          text: '已选择的选项',
+          data: '1',
+        },
+        {
+          text: '短的选项二',
+          data: '2',
+        },
+        {
+          text: '很长很长很长的选项三',
+          data: '3',
+        },
+      ],
+      value: '1',
+    };
+  },
+  methods: {
+    handleChange(value) {
+      console.log(value);
+    },
+    visibleChange(val) {
+      console.log('visible', val);
+    },
+  },
+};
+</script>
+<style scoped>
+.demo-select-base {
+  width: 200px;
+  display: inline-block;
+  margin: 0 20px;
+}
+</style>
