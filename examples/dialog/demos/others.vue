30768df6391b23fe6d9fe0b63c3441a9cc1b2c63
LeeJim <273743552@qq.com>
2021-06-30 15:51:39 +0800

    chore(dialog): update demo code

--- examples/dialog/demos/others.vue
+++ examples/dialog/demos/others.vue
@@ -14,107 +14,107 @@
     <br>
 
     <t-dialog
-      :visible.sync="visibleBody"
+      v-model:visible="visibleBody"
       attach="body"
       header="挂载在body"
       :onClickConfirm="()=>this.visibleBody = false"
     >
-      <div slot="body">
+      <template #body>
         <div>我是被挂载到body元素的对话框</div>
         <div>我是内容</div>
         <div>我是内容</div>
         <div>我是内容</div>
         <div>我是内容</div>
-      </div>
+      </template>
     </t-dialog>
 
     <t-dialog
-      :visible.sync="visibleIdAttach"
+      v-model:visible="visibleIdAttach"
       attach="#app"
       header="挂载到id为app的元素"
       :onClickConfirm="()=>this.visibleIdAttach = false"
     >
-      <div slot="body">
+      <template #body>
         <div>通过querySelect指定元素挂载</div>
         <div>支持原生document.querySelect选择元素</div>
         <div>querySelect获取到的第一个元素为挂载点</div>
         <div>我是内容</div>
         <div>我是内容</div>
-      </div>
+      </template>
     </t-dialog>
 
     <t-dialog
-      :visible.sync="visibleFunctionAttach"
+      v-model:visible="visibleFunctionAttach"
       :attach="getAttach"
       header="函数返回挂载节点"
       :onClickConfirm="()=>this.visibleFunctionAttach = false"
     >
-      <div slot="body">
+      <template #body>
         <div>指定函数返回的节点为挂载点</div>
         <div>函数返回为DOM节点对象</div>
         <div>我是内容</div>
         <div>我是内容</div>
         <div>我是内容</div>
-      </div>
+      </template>
     </t-dialog>
 
     <t-dialog
       header="模态对话框"
-      :visible.sync="visibleModal"
+      v-model:visible="visibleModal"
       mode="modal"
       draggable
       :onClickConfirm="()=>this.visibleModal = false"
     >
-      <div slot="body">
+      <template #body>
         <div>默认点击蒙层或按ESC可关闭</div>
         <div>我是内容</div>
         <div>我是内容</div>
-      </div>
+      </template>
     </t-dialog>
 
     <t-dialog
       header="普通对话框"
-      :visible.sync="visibleNormal"
+      v-model:visible="visibleNormal"
       mode="normal"
       :onClickConfirm="()=>this.visibleNormal = false"
     >
-      <div slot="body">
+      <template #body>
         <div>普通对话框不可拖拽</div>
         <div>我是内容</div>
         <div>我是内容</div>
-      </div>
+      </template>
     </t-dialog>
 
     <t-dialog
       header="非模态对话框-可拖拽"
-      :visible.sync="visibleModelessDrag"
+      v-model:visible="visibleModelessDrag"
       mode="modeless"
       draggable
       :onClickConfirm="()=>this.visibleModelessDrag = false"
     >
-      <div slot="body">
+      <template #body>
         <div>我是内容</div>
         <div>我是内容</div>
-      </div>
+      </template>
     </t-dialog>
 
     <t-dialog
       header="非模态对话框-不可拖拽"
-      :visible.sync="visibleModeless"
+      v-model:visible="visibleModeless"
       mode="modeless"
       :onClickConfirm="()=>this.visibleModeless = false"
     >
-      <div slot="body">
+      <template #body>
         <div>我是内容</div>
         <div>我是内容</div>
-      </div>
+      </template>
     </t-dialog>
 
   </div>
 </template>
 <script lang="ts">
-import Vue from 'vue';
-export default Vue.extend({
+import { defineComponent } from 'vue';
+export default defineComponent({
   data() {
     return {
       visibleBody: false,
